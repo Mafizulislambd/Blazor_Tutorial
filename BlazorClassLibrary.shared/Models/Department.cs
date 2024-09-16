fc3b9a9d9a469a991176ce3f411c08866f715271
Mafizulislambd <167568030+Mafizulislambd@users.noreply.github.com>
2024-09-16 10:37:23 +0600

    Project update git commit -m update

--- BlazorClassLibrary.shared/Models/Department.cs
+++ BlazorClassLibrary.shared/Models/Department.cs
@@ -8,5 +8,7 @@ namespace BlazorClassLibrary.shared.Models
 {
     public class Department
     {
+        public int DepartmentId { get; set; }
+        public string DepartmentName { get; set; }
     }
 }
--- BlazorClassLibrary.shared/Models/Employee.cs
+++ BlazorClassLibrary.shared/Models/Employee.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,13 +10,16 @@ namespace BlazorClassLibrary.shared.Models
     public class Employee
     {
         public int EmployeeId { get; set; }
-        public string? FirstName { get; set; }
-        public string? LastName { get; set; }
-        public string? Email { get; set; }
+        [Required]
+        [MinLength(2, ErrorMessage = "FirstName must contains at least 2 charcters")]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        public string Email { get; set; }
         public DateTime DateOfBrith { get; set; }
         public Gender Gender { get; set; }
         public int DepartmentId { get; set; }
-        public string? PhotoPath { get; set; }
-        public Department? Department { get; set; }
+        public string PhotoPath { get; set; }
+        public Department Department { get; set; }
     }
 }
