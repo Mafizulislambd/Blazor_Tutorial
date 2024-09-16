c6639cc66d9703cd4fa1101c7b7357770dee6471
Mafizulislambd <167568030+Mafizulislambd@users.noreply.github.com>
2024-09-16 08:37:05 +0600

    Project update

--- BlazorWASApp/Program.cs
+++ BlazorWASApp/Program.cs
@@ -1,11 +1,14 @@
 using BlazorWASApp;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Syncfusion.Blazor;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+#region new add by mafizul
+builder.Services.AddSyncfusionBlazor();
+#endregion
 await builder.Build().RunAsync();
